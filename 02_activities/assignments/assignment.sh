e95195503de8ef02ee1384d3153c601abd958915
zhuoma <yongci.zhuoma@gmail.com>
2024-12-01 21:26:33 -0500

    update changes

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -18,7 +18,7 @@ curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_ac
 unzip rawdata.zip
 
 ###########################################
-# Complete assignment here
+# Complete assignment here . 
 
 # 1. Create a directory named data
 mkdir data
@@ -30,7 +30,7 @@ mv ./rawdata ./data/raw
 ls ./data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-mkdir ./data/proce
+mkdir ./data/processed server_logs ./data/processed user_logs ./data/processed event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 cp ./data/raw/./* .log ./data/processed/server_logs
